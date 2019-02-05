497868899863cb550dfec88175034da48f888f32
atkinsonholly <atkinsonholly@gmail.com>
2019-02-05 22:40:59 +0000

    Pokemon render - need to add functionality

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -2,14 +2,13 @@ import React from 'react'
 import PokemonCard from './PokemonCard'
 import { Card } from 'semantic-ui-react'
 
-class PokemonCollection extends React.Component {
-  render() {
-    return (
-      <Card.Group itemsPerRow={6}>
-        <h1>Hello From Pokemon Collection</h1>
-      </Card.Group>
-    )
-  }
+const PokemonCollection = (props) => {
+  return (
+    <Card.Group itemsPerRow={6} className='ui cards' >
+      <h1>Hello From Pokemon Collection</h1>
+      { props.pokemonToRender.map(card => <PokemonCard key={card.id} pokemon={card}/>) }
+    </Card.Group>
+  )
 }
 
 export default PokemonCollection
--- src/components/PokemonIndex.js
+++ src/components/PokemonIndex.js
@@ -1,10 +1,62 @@
-import React from 'react'
+import React, {Component} from 'react'
 import PokemonCollection from './PokemonCollection'
 import PokemonForm from './PokemonForm'
 import { Search } from 'semantic-ui-react'
 import _ from 'lodash'
 
-class PokemonPage extends React.Component {
+const API = 'http://localhost:3000/pokemon'
+
+export default class PokemonPage extends Component {
+
+  state = {
+    pokemon: []  
+  }
+
+  fetchPokemon = async(API) => {
+    return await fetch(API)
+    .then(response => response.json())
+  }
+
+  async componentDidMount() {
+    this.renderPokemon()
+  }
+
+  async renderPokemon(){
+    const pokemonToParse = await this.fetchPokemon(API)
+
+    if (pokemonToParse.length > 0) {
+
+      //console.log(pokemonToParse[0].name)
+
+      const newPokemon = pokemonToParse.map( data => ({
+        id: data.id,
+        name: data.name,
+        hp: data.stats[5].value,
+        frontUrl: data.sprites.front,
+        backUrl: data.sprites.back,
+        toggle: false
+      })
+      )
+      //console.log(newPokemon[0])
+
+      this.setState({
+        pokemon: newPokemon
+      })
+      console.log(this.state.pokemon[0])
+    }
+  }
+
+  filteredPokemon = (event) => {
+    event.persist()
+    const searchInput = event.target.value
+    const copyPokemon = [...this.state.pokemon]
+    this.setState({
+      searchTerm: searchInput
+    })
+    return copyPokemon.filter( pokemon => pokemon.name.includes(searchInput ))
+  }
+
+
   render() {
     return (
       <div>
@@ -12,12 +64,11 @@ class PokemonPage extends React.Component {
         <br />
         <Search onSearchChange={_.debounce(() => console.log('🤔'), 500)} showNoResults={false} />
         <br />
-        <PokemonCollection />
+        <PokemonCollection pokemonToRender={this.state.pokemon} />
         <br />
         <PokemonForm />
       </div>
     )
   }
-}
 
-export default PokemonPage
+}
